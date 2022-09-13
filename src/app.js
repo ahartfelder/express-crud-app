eea6dbbaf4eb9945f53de6afcb06d5636b09dcd2
ahartfelder <74719748+ahartfelder@users.noreply.github.com>
2022-09-13 20:09:31 -0300

    router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 const express = require('express')
+const router = require('./routes/routes')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.use(router)
 
 const listener = app.listen(3000, () => {
   console.log(`App is running on port ${listener.address().port}`)
